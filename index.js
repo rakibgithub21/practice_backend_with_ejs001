c1581e533ea978da9df6049d65007e73712053d4
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-12-08 07:53:48 +0600

    make signin register profile page

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+
+//navigate register , login , profile page
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+app.get('/signin', (req, res) => {
+    res.render('signin')
+})
+app.get('/profile', (req, res) => {
+    res.render('profile')
+})
+
 app.listen(3000, () => {
     console.log('server is running on port 3000');
 })
