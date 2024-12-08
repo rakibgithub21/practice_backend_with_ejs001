6eb63303253c3d897b1bf27fe834e050da1f4a07
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-12-08 11:23:17 +0600

    make register api to register and user bcrypt for password

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express()
+const userModel = require('./models/user.model');
+const bcrypt = require('bcrypt');
 
 app.set('view engine', "ejs") // for view engine
 app.use(express.urlencoded({ extended: true })) // for get form data
 
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -25,3 +27,58 @@ app.get('/profile', (req, res) => {
 app.listen(3000, () => {
     console.log('server is running on port 3000');
 })
+
+
+// register
+
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+    const isUsernameExist = await userModel.findOne({ username })
+    const isEmailExist = await userModel.findOne({ email })
+
+    if (isUsernameExist || isEmailExist) {
+        return res.status(409).send('try unique email or username for your account')
+    }
+
+
+    // for hash password
+    bcrypt.genSalt(10, function (err, salt) {
+
+        bcrypt.hash(password, salt, async function (err, hash) {
+            
+            const user = await userModel.create({
+                username,
+                email,
+                password: hash
+            })
+            
+            res.send(user)
+        });
+
+    });
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/users')
+
+
+const userSchema = mongoose.Schema({
+    username: String,
+    email: String,
+    password:String,
+})
+
+const user = mongoose.model('user',userSchema)
+module.exports = user
