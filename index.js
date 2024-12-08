fe6e34b4c5090d2cd62bef2acbc0176a1e1417af
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-12-08 07:32:34 +0600

    make a ejs page

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express()
 
+app.set('view engine', "ejs") // for view engine
+app.use(express.urlencoded({ extended: true })) // for get form data
+
+
+
 app.get('/', (req, res) => {
-    res.send('checking server')
+    res.render('index')
 })
 
 app.listen(3000, () => {
